c222e87731c3eb9a0bcf537008b2ff97b4346411
Ikamaz <mazmishviliika@gmail.com>
2024-11-02 23:21:25 +0400

    Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.carousel-slide');
+let currentIndex = 0;
+
+function initializeCarousel() {
+  const slides = document.querySelectorAll('.carousel-slide');
+  if (slides.length === 0) return;
+
+  let currentIndex = 0;
+
+  function showNextSlide() {
+      slides[currentIndex].style.opacity = '0';
+      currentIndex = (currentIndex + 1) % slides.length;
+      slides[currentIndex].style.opacity = '1';
+  }
+
+  slides[currentIndex].style.opacity = '1';
+  setInterval(showNextSlide, 5000);
+}
+
+
+document.querySelector('#burger').addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.nav-links').classList.toggle('active');
+  document.querySelector('.nav-product').classList.toggle('active');
+});
